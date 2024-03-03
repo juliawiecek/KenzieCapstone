0fdfa601f2ebf3977c2ae606f39bb0daf711e497
kfactor072 <kfactor072@gmail.com>
2024-03-03 15:43:48 -0500

    comment service and model created

---
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/CommentRecord.java
@@ -0,0 +1,85 @@
+package com.kenzie.appserver.repositories.model;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import java.util.Objects;
+
+@DynamoDBTable(tableName = "Comment")
+public class CommentRecord {
+    private String commentId;
+    private String userId;
+    private String title;
+    private String contents;
+    private String episodeId;
+    private String timestamp;
+
+    @DynamoDBHashKey(attributeName = "CommentId")
+    public String getCommentId() {
+        return commentId;
+    }
+
+    @DynamoDBRangeKey(attributeName = "UserId")
+    public String getUserId() {
+        return userId;
+    }
+
+    @DynamoDBAttribute(attributeName = "Title")
+    public String getTitle(){
+        return title;
+    }
+
+    @DynamoDBAttribute(attributeName = "Contents")
+    public String getContents(){
+        return contents;
+    }
+
+    @DynamoDBAttribute(attributeName = "EpisodeId")
+    public String getEpisodeId(){
+        return episodeId;
+    }
+
+    @DynamoDBAttribute(attributeName = "Timestamp")
+    public String getTimestamp(){
+        return timestamp;
+    }
+
+    public void setCommentId(String commentId) {
+        this.commentId = commentId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public void setEpisodeId(String episodeId) {
+        this.episodeId = episodeId;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CommentRecord that = (CommentRecord) o;
+        return Objects.equals(commentId, that.commentId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(commentId);
+    }
+}
