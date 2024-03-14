c71d16f2826b302a1e8fbe643db8ba0276adb303
josephallheart <jrrese01@gmail.com>
2024-03-14 19:06:33 -0400

    Create, partially initialize EpisodeResponse, may use for seasons too

--- ServiceLambda/src/main/java/com/kenzie/capstone/service/model/EpisodeResponse.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/model/EpisodeResponse.java
@@ -1,4 +1,20 @@
 package com.kenzie.capstone.service.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude
 public class EpisodeResponse {
+    @JsonProperty("name")
+    private String name;
+//    @JsonProperty("episodeOrder")
+//    private int episodeOrder;
+    @JsonProperty("number")
+    private int number;
+    @JsonProperty("runtime")
+    private int runtime;
+    @JsonProperty("image")
+    private String image;
+    @JsonProperty("summary")
+    private String summary;
 }
