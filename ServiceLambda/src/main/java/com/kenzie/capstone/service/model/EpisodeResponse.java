475bcd3ff42d78e45514663ea440bd9617aa880d
josephallheart <jrrese01@gmail.com>
2024-03-15 18:16:54 -0400

    Finish EpisodeResponse

--- ServiceLambda/src/main/java/com/kenzie/capstone/service/model/EpisodeResponse.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/model/EpisodeResponse.java
@@ -3,12 +3,12 @@ package com.kenzie.capstone.service.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonInclude
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class EpisodeResponse {
     @JsonProperty("name")
     private String name;
-//    @JsonProperty("episodeOrder")
-//    private int episodeOrder;
+    @JsonProperty("episodeOrder")
+    private int episodeOrder;
     @JsonProperty("number")
     private int number;
     @JsonProperty("runtime")
@@ -17,4 +17,52 @@ public class EpisodeResponse {
     private String image;
     @JsonProperty("summary")
     private String summary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEpisodeOrder() {
+        return episodeOrder;
+    }
+
+    public void setEpisodeOrder(int episodeOrder) {
+        this.episodeOrder = episodeOrder;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
 }
