dbf7ebc29a2f39eab13227db72f57d8ba05282aa
Julia Wiecek <julia.wiecek@mykenzie.snhu.edu>
2024-03-18 20:02:06 -0500

    made changes to service classes, experimented with structure

--- ServiceLambdaJavaClient/src/main/java/com/kenzie/capstone/service/client/TVShowServiceClient.java
+++ ServiceLambdaJavaClient/src/main/java/com/kenzie/capstone/service/client/TVShowServiceClient.java
@@ -1,53 +1,96 @@
 package com.kenzie.capstone.service.client;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kenzie.capstone.service.dao.TVShowDao;
-package com.kenzie.capstone.service.client;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kenzie.capstone.service.dao.TVShowDao;
-import com.kenzie.capstone.service.model.TVShowData;
+import com.kenzie.capstone.service.model.*;
 
 
 public class TVShowServiceClient {
 
     private static final String GET_POPULAR_SHOWS_ENDPOINT = "/shows";
-    private static final String SEARCH_SHOWS_ENDPOINT = "/search/shows?q=";
-    private static final String GET_SHOW_INFO_ENDPOINT = "/shows/";
-    private static final String GET_SHOW_EPISODE_LIST_ENDPOINT = "/shows/";
+    private static final String SEARCH_SHOWS_ENDPOINT = "/search/shows?q={query}";
+    private static final String GET_SHOW_INFO_ENDPOINT = "/shows/{id}";
+    private static final String GET_SHOW_IMAGES_ENDPOINT = "/shows/{id}/images";
+    private static final String GET_SHOW_SEASONS_ENDPOINT = "/shows/{id}/seasons";
+    private static final String GET_SHOW_EPISODE_LIST_ENDPOINT = "/seasons/{id}/episodes";
 
     private ObjectMapper mapper;
-    private TVShowDao tvShowDao;
 
-    public TVShowServiceClient(TVShowDao tvShowDao) {
-        this.tvShowDao = tvShowDao;
+    public TVShowServiceClient() {
         this.mapper = new ObjectMapper();
     }
 
-    public TVShowData getPopularShows() {
-        String response = tvShowDao.makeApiRequest(GET_POPULAR_SHOWS_ENDPOINT);
-        return mapJsonToTVShowData(response);
-    }
-
-    public TVShowData searchShows(String query) {
-        String response = tvShowDao.makeApiRequest(SEARCH_SHOWS_ENDPOINT + query);
-        return mapJsonToTVShowData(response);
-    }
-
-    public TVShowData getShowInfo(String id) {
-        String response = tvShowDao.makeApiRequest(GET_SHOW_INFO_ENDPOINT + id + "?embed[]=episodes&embed[]=images");
-        return mapJsonToTVShowData(response);
-    }
-
-    public TVShowData getShowEpisodeList(String id) {
-        String response = tvShowDao.makeApiRequest(GET_SHOW_EPISODE_LIST_ENDPOINT + id + "/episodes");
-        return mapJsonToTVShowData(response);
-    }
-
-    private TVShowData mapJsonToTVShowData(String json) {
+    public ShowInfoData getPopularShows() {
+        EndpointUtility endpointUtility = new EndpointUtility();
+        String response = endpointUtility.getEndpoint(GET_POPULAR_SHOWS_ENDPOINT);
+        ShowInfoData showInfoData;
         try {
-            return mapper.readValue(json, TVShowData.class);
+            showInfoData = mapper.readValue(response, ShowInfoData.class);
         } catch (Exception e) {
             throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
         }
+        return showInfoData;
+    }
+
+    public ShowInfoResponse searchShows(String query) {
+        EndpointUtility endpointUtility = new EndpointUtility();
+        String response = endpointUtility.getEndpoint(SEARCH_SHOWS_ENDPOINT.replace("{query}", query));
+        ShowInfoResponse showInfoResponse;
+        try {
+            showInfoResponse = mapper.readValue(response, ShowInfoResponse.class);
+        } catch (Exception e) {
+            throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
+        }
+        return showInfoResponse;
+    }
+
+    public ShowInfoData getShowInfo(String id) {
+        EndpointUtility endpointUtility = new EndpointUtility();
+        String response = endpointUtility.getEndpoint(GET_SHOW_INFO_ENDPOINT.replace("{id}", id));
+        ShowInfoData showInfoData;
+        try {
+            showInfoData = mapper.readValue(response, ShowInfoData.class);
+        } catch (Exception e) {
+            throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
+        }
+        return showInfoData;
+    }
+
+    public ImageData getShowImages(String id) {
+        EndpointUtility endpointUtility = new EndpointUtility();
+        String response = endpointUtility.getEndpoint(GET_SHOW_IMAGES_ENDPOINT.replace("{id}", id));
+        ImageData imageData;
+        try {
+            imageData = mapper.readValue(response, ImageData.class);
+        } catch (Exception e) {
+            throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
+        }
+        return imageData;
+    }
+
+    public EpisodeData getShowSeasons(String id) {
+        EndpointUtility endpointUtility = new EndpointUtility();
+        String response = endpointUtility.getEndpoint(GET_SHOW_SEASONS_ENDPOINT.replace("{id}", id));
+        EpisodeData episodeData;
+        try {
+            episodeData = mapper.readValue(response, EpisodeData.class);
+        } catch (Exception e) {
+            throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
+        }
+        return episodeData;
+    }
+
+    public EpisodeData getShowEpisodeList(String id) {
+        EndpointUtility endpointUtility = new EndpointUtility();
+        String response = endpointUtility.getEndpoint(GET_SHOW_EPISODE_LIST_ENDPOINT.replace("{id}", id));
+        EpisodeData episodeData;
+        try {
+            episodeData = mapper.readValue(response, EpisodeData.class);
+        } catch (Exception e) {
+            throw new ApiGatewayException("Unable to map deserialize JSON: " + e);
+        }
+        return episodeData;
     }
 }
