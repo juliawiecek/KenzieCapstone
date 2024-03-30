f723562f365dc371d92b0678fdb88aeee808babb
egrok99 <egor.olender@mykenzie.snhu.edu>
2024-03-30 17:12:54 -0400

    Fixed unit test for TVShowService and resolved issue with bootRunDev

--- ServiceLambda/src/main/java/com/kenzie/capstone/service/lambda/GetShow.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/lambda/GetShow.java
@@ -7,7 +7,7 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.kenzie.capstone.service.TVShowService;
-import com.kenzie.capstone.service.dependency.DaggerServiceComponent;
+import com.kenzie.capstone.service.dependency.DaggerTVShowServiceComponent;
 import com.kenzie.capstone.service.dependency.TVShowServiceComponent;
 import com.kenzie.capstone.service.model.ShowInfoData;
 import com.kenzie.capstone.service.model.ShowInfoResponse;
@@ -27,7 +27,7 @@ public class GetShow implements RequestHandler<APIGatewayProxyRequestEvent, APIG
 
         log.info(gson.toJson(input));
 
-        TVShowServiceComponent tvShowServiceComponent = (TVShowServiceComponent) DaggerServiceComponent.create();
+        TVShowServiceComponent tvShowServiceComponent = (TVShowServiceComponent) DaggerTVShowServiceComponent.create();
         TVShowService tvShowService = tvShowServiceComponent.provideTVShowService();
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
